1f57a283dfaf0626aa80788dc0cbaa1a0c39ef88
Edward Wu <bluehome.wu@gmail.com>
2022-05-14 03:24:08 +0800

    Z01R:temp
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -62,8 +62,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
